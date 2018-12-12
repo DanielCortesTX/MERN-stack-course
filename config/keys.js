c633f43a165862fd04b819162df5d22076c3445a
Daniel Cortes <d5t5c5@yahoo.com>
2018-12-12 11:17:49 -0600

    feat: initial commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://testing123:testing123@ds123532.mlab.com:23532/devmernconnector'
+  mongoURI: 'mongodb://testing123:testing123@ds123532.mlab.com:23532/devmernconnector',
+  secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
 
 const app = express()
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // DB Config
 const db = require('./config/keys').mongoURI
@@ -16,11 +22,15 @@ mongoose
   .then(() => console.log('mongodb connected'))
   .catch((err) => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World'))
+// passport middleware
+app.use(passport.initialize())
 
-app.use('api/users', users)
-app.use('api/profile', profile)
-app.use('api/posts', posts)
+// passport Config
+require('./config/passport')(passport)
+
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000
 
